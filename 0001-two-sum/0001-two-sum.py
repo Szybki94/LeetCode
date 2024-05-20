56abbe4ac3e5f859e9546d94265cb2cda239379d
Szybki94 <90515495+Szybki94@users.noreply.github.com>
2024-05-20 22:05:35 +0200

    Delete 0001-two-sum directory
    This was just for testing :)

--- 0001-two-sum/0001-two-sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_to_index = {}
-        for i, j in enumerate(nums):
-            complement = target - j
-            if complement in num_to_index:
-                return [num_to_index[complement], i]
-            num_to_index[j] = i
