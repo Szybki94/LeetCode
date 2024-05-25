e4e737423927632b7ebd42d04c1ae4972d60b97a
Szybki94 <90515495+Szybki94@users.noreply.github.com>
2024-05-25 14:10:11 +0200

    Time: 1703 ms (18.90%), Space: 17.3 MB (98.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,11 @@
-# First thought solution
+# Second thought solution
+# There is no need to repeat going through the same number in every loop
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for idx_1, value_1 in enumerate(nums):
-            for idx_2, value_2 in enumerate(nums):
-                if idx_1 == idx_2:
-                    continue
-                elif (value_1 + value_2) == target:
+            for idx_2, value_2 in enumerate(nums[idx_1+1:], idx_1+1): # Here is added a little optimization
+                if (value_1 + value_2) == target:
                     return [idx_1, idx_2]
+
+# But is still O(n^2) solution :/
             
